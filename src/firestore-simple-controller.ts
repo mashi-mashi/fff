c23f50b27d6f9c2aa35713a5ae6b00ca0384fcbd
mashi-mashi <shima.trends@gmail.com>
2021-03-03 00:56:48 +0900

    Implement firestore-controller

---
+++ src/firestore-simple-controller.ts
@@ -0,0 +1,32 @@
+import {Request, Response} from 'express-serve-static-core';
+
+import {sendResponse} from './api/api-utils';
+import {FirestoreDao} from './firestore-dao';
+import {LoggerInterface} from './logger/logger';
+import {FirestoreDocumentType, NestedPartial, OptionalId} from './types/firestore-types';
+
+export class FirestoreSimpleController<T extends FirestoreDocumentType> {
+  private dao: FirestoreDao<T>;
+  private collectionPath: string;
+
+  private constructor({collectionPath}: {collectionPath: string}) {
+    this.collectionPath = collectionPath;
+    this.dao = FirestoreDao.initialize<T>({path: collectionPath});
+  }
+
+  public static initialize<U extends FirestoreDocumentType>({collectionPath}: {collectionPath: string}) {
+    return new FirestoreSimpleController<U>({collectionPath});
+  }
+
+  public create = async (req: Request, res: Response, data: OptionalId<T>) => {
+    return sendResponse(req, res, this.dao.add(data));
+  };
+
+  public update = async (req: Request, res: Response, data: NestedPartial<T> & {id: string}) => {
+    return sendResponse(req, res, this.dao.set(data));
+  };
+
+  public delete = async (req: Request, res: Response, id: string) => {
+    return sendResponse(req, res, this.dao.delete(id));
+  };
+}
