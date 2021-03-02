53694db8ff6125df234e423460310c69ea7bc06e
mashi-mashi <shima.trends@gmail.com>
2021-03-03 00:35:46 +0900

    Implement api-commons

---
+++ src/api/api-utils.ts
@@ -0,0 +1,25 @@
+import {Request, Response} from 'express-serve-static-core';
+import {DeepTimestampToMillis} from '../types/firestore-types';
+import {deepTimestampToMillis} from '../utils/utils';
+import {HttpError, HttpStatusCode} from './http-error';
+import {loggerAndUser} from './api-middlware';
+
+export const sendResponse = <T>(req: Request, res: Response<DeepTimestampToMillis<T>>, result: Promise<T>) => {
+  const {logger} = loggerAndUser(req);
+  result
+    .then(data => {
+      const result = deepTimestampToMillis(data);
+      logger?.log('api-result', result);
+      res.send(result);
+    })
+    .catch(e => {
+      logger?.error('error', e);
+      if (e instanceof HttpError) {
+        res.status(e.status).send(e.param as any);
+      } else {
+        res
+          .status(HttpStatusCode.INTERNAL_SERVER_ERROR)
+          .send({code: 'internal', message: 'internal-server-error'} as any);
+      }
+    });
+};
