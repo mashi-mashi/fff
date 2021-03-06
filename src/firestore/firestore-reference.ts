55ca5adb420925e7ddb38a94a49dd897bf616eed
mashi-mashi <shima.trends@gmail.com>
2021-03-07 02:44:05 +0900

    Update firestore-ref

--- src/firestore/firestore-reference.ts
+++ src/firestore/firestore-reference.ts
@@ -5,15 +5,33 @@ import {Firestore} from './firestore';
 type WithId<T> = T & {id: string};
 
 export class FirestoreReference<T> {
-  private path: string;
-  constructor(...paths: string[]) {
-    this.path = FFF.firestoreRootPath + paths.join('/');
+  private name: string;
+  constructor(name: string) {
+    this.name = FFF.firestoreRootPath + name;
   }
 
-  public getPath = (): string => this.path;
+  public getPath = (): string => Firestore.collection(this.name).path;
 
   public collection = (): CollectionReference<WithId<T>> =>
-    Firestore.collection(this.path) as CollectionReference<WithId<T>>;
+    Firestore.collection(this.name) as CollectionReference<WithId<T>>;
   public doc = (id: string): DocumentReference<WithId<T>> => this.collection().doc(id) as DocumentReference<WithId<T>>;
   public newDoc = (): DocumentReference<WithId<T>> => this.collection().doc() as DocumentReference<WithId<T>>;
+}
+
+export class FirestoreNestReference<T> {
+  private name: string;
+  private parentPath: (parentId: string) => DocumentReference<any>;
+  constructor(parentRef: FirestoreReference<any>, name: string) {
+    this.parentPath = (parentId: string) => parentRef.doc(parentId);
+    this.name = name;
+  }
+
+  public getPath = (parentId: string): string => this.parentPath(parentId).collection(this.name).path;
+
+  public collection = (parentId: string): CollectionReference<WithId<T>> =>
+    this.parentPath(parentId).collection(this.name) as CollectionReference<WithId<T>>;
+  public doc = (parentId: string, id: string): DocumentReference<WithId<T>> =>
+    this.parentPath(parentId).collection(this.name).doc(id) as DocumentReference<WithId<T>>;
+  public newDoc = (parentId: string): DocumentReference<WithId<T>> =>
+    this.parentPath(parentId).collection(this.name).doc() as DocumentReference<WithId<T>>;
 }
