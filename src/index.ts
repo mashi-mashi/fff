9f1642d24172aa6f8edf2d78c3a7b7dd8f10dc18
mashi-mashi <shima.trends@gmail.com>
2021-03-06 17:35:40 +0900

    add index

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ export {FFF} from './fff';
 export {Firestore} from './firestore/firestore';
 export {FirestoreBatch} from './firestore/firestore-batch';
 export {FirestoreReference} from './firestore/firestore-reference';
+export {Functions} from './functions/firebase-functions';
 export {Logger} from './logger/logger';
+export {Pubsub} from './pubsub/pubsub';
 export {ApiHelper, ApiMiddlware, HttpError, HttpStatusCode, HttpStatusType};
